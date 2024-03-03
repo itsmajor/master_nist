cee228f3e49bbad6400e3ec6c22cfd3c7f758e27
Tomas <tomas.itsmajor@gmail.com>
2024-03-03 19:19:07 +0100

    disable ASM cycle count in SABER

--- SABER-Round3/Reference_Implementation_KEM/test/cpucycles.c
+++ SABER-Round3/Reference_Implementation_KEM/test/cpucycles.c
@@ -2,8 +2,9 @@
 
 long long cpucycles(void)
 {
-  unsigned long long result;
-  asm volatile(".byte 15;.byte 49;shlq $32,%%rdx;orq %%rdx,%%rax"
-    : "=a" (result) ::  "%rdx");
-  return result;
+//  unsigned long long result;
+//  asm volatile(".byte 15;.byte 49;shlq $32,%%rdx;orq %%rdx,%%rax"
+//    : "=a" (result) ::  "%rdx");
+//  return result;
+    return 0;
 }
