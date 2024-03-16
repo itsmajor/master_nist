17d62a74bf7def93a3ede5fdc253fb1af1525270
Tomas <tomas.itsmajor@gmail.com>
2024-03-16 22:41:47 +0100

    build_all

--- BIG_QUAKE-Round1/build_all.sh
+++ BIG_QUAKE-Round1/build_all.sh
@@ -1,14 +1,14 @@
 cd Optimized_Implementation/BIG_QUAKE_1
 echo "moved to: ${PWD}"
 make all
-cd ../../..
+cd ../..
 
 cd Optimized_Implementation/BIG_QUAKE_3
 echo "moved to: ${PWD}"
 make all
-cd ../../..
+cd ../..
 
 cd Optimized_Implementation/BIG_QUAKE_5
 echo "moved to: ${PWD}"
 make all
-cd ../../..
+cd ../..
--- CRYSTALS_Dilithium-Round3/build_all.sh
+++ CRYSTALS_Dilithium-Round3/build_all.sh
@@ -1,59 +1,59 @@
-cd dilithium2
+cd Reference_Implementation/crypto_sign/dilithium2
 echo "moved to: ${PWD}"
 make speed
-cd ..
+cd ../../..
 
-cd dilithium2-AES
+cd Reference_Implementation/crypto_sign/dilithium2-AES
 echo "moved to: ${PWD}"
 make speed
-cd ..
+cd ../../..
 
-cd dilithium2-AES-R
+cd Reference_Implementation/crypto_sign/dilithium2-AES-R
 echo "moved to: ${PWD}"
 make speed
-cd ..
+cd ../../..
 
-cd dilithium2-R
+cd Reference_Implementation/crypto_sign/dilithium2-R
 echo "moved to: ${PWD}"
 make speed
-cd ..
+cd ../../..
 
-cd dilithium3
+cd Reference_Implementation/crypto_sign/dilithium3
 echo "moved to: ${PWD}"
 make speed
-cd ..
+cd ../../..
 
-cd dilithium3-AES
+cd Reference_Implementation/crypto_sign/dilithium3-AES
 echo "moved to: ${PWD}"
 make speed
-cd ..
+cd ../../..
 
-cd dilithium3-AES-R
+cd Reference_Implementation/crypto_sign/dilithium3-AES-R
 echo "moved to: ${PWD}"
 make speed
-cd ..
+cd ../../..
 
-cd dilithium3-R
+cd Reference_Implementation/crypto_sign/dilithium3-R
 echo "moved to: ${PWD}"
 make speed
-cd ..
+cd ../../..
 
-cd dilithium5
+cd Reference_Implementation/crypto_sign/dilithium5
 echo "moved to: ${PWD}"
 make speed
-cd ..
+cd ../../..
 
-cd dilithium5-AES
+cd Reference_Implementation/crypto_sign/dilithium5-AES
 echo "moved to: ${PWD}"
 make speed
-cd ..
+cd ../../..
 
-cd dilithium5-AES-R
+cd Reference_Implementation/crypto_sign/dilithium5-AES-R
 echo "moved to: ${PWD}"
 make speed
-cd ..
+cd ../../..
 
-cd dilithium5-R
+cd Reference_Implementation/crypto_sign/dilithium5-R
 echo "moved to: ${PWD}"
 make speed
-cd ..
+cd ../../..
