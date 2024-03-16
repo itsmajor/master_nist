e2e478a9d2add422ec8ad4d36a6a016e12fa5715
Tomas <tomas.itsmajor@gmail.com>
2024-03-16 16:21:39 +0100

    build_all

--- BIG_QUAKE-Round1/build_all.sh
+++ BIG_QUAKE-Round1/build_all.sh
@@ -1,6 +1,9 @@
 cd Optimized_Implementation/BIG_QUAKE_1
+echo "moved to: ${PWD}"
 make all
 cd ../../Optimized_Implementation/BIG_QUAKE_3
+echo "moved to: ${PWD}"
 make all
 cd ../../Optimized_Implementation/BIG_QUAKE_5
+echo "moved to: ${PWD}"
 make all
--- build_all.sh
+++ build_all.sh
@@ -1,5 +1,6 @@
 #cd /BIG_QUAKE-Round1
 cd BIG_QUAKE-Round1
+echo "moved to: ${PWD}"
 ./build_all.sh
 
 echo "**** done ****"
