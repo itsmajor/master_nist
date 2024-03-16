31367faf59ce0a76c20f6f1241d41a7f0ce3a7fc
Tomas <tomas.itsmajor@gmail.com>
2024-03-16 22:37:44 +0100

    build_all

--- BIG_QUAKE-Round1/build_all.sh
+++ BIG_QUAKE-Round1/build_all.sh
@@ -1,9 +1,14 @@
 cd Optimized_Implementation/BIG_QUAKE_1
 echo "moved to: ${PWD}"
 make all
-cd ../../Optimized_Implementation/BIG_QUAKE_3
+cd ../../..
+
+cd Optimized_Implementation/BIG_QUAKE_3
 echo "moved to: ${PWD}"
 make all
-cd ../../Optimized_Implementation/BIG_QUAKE_5
+cd ../../..
+
+cd Optimized_Implementation/BIG_QUAKE_5
 echo "moved to: ${PWD}"
 make all
+cd ../../..
---
+++ CRYSTALS_Dilithium-Round3/build_all.sh
@@ -0,0 +1,59 @@
+cd dilithium2
+echo "moved to: ${PWD}"
+make speed
+cd ..
+
+cd dilithium2-AES
+echo "moved to: ${PWD}"
+make speed
+cd ..
+
+cd dilithium2-AES-R
+echo "moved to: ${PWD}"
+make speed
+cd ..
+
+cd dilithium2-R
+echo "moved to: ${PWD}"
+make speed
+cd ..
+
+cd dilithium3
+echo "moved to: ${PWD}"
+make speed
+cd ..
+
+cd dilithium3-AES
+echo "moved to: ${PWD}"
+make speed
+cd ..
+
+cd dilithium3-AES-R
+echo "moved to: ${PWD}"
+make speed
+cd ..
+
+cd dilithium3-R
+echo "moved to: ${PWD}"
+make speed
+cd ..
+
+cd dilithium5
+echo "moved to: ${PWD}"
+make speed
+cd ..
+
+cd dilithium5-AES
+echo "moved to: ${PWD}"
+make speed
+cd ..
+
+cd dilithium5-AES-R
+echo "moved to: ${PWD}"
+make speed
+cd ..
+
+cd dilithium5-R
+echo "moved to: ${PWD}"
+make speed
+cd ..
