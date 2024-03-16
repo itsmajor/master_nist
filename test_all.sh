9bb384e4ecd255b94eba37cfffdb35976b399beb
Tomas <tomas.itsmajor@gmail.com>
2024-03-16 22:55:33 +0100

    build_all

--- test_all.sh
+++ test_all.sh
@@ -1,146 +1,146 @@
 cd BIG_QUAKE-Round1
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd BIKE-Round4
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd Classic-McEliece-Round4
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd CRYSTALS_Dilithium-Round3
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd CRYSTALS_Kyber-Round3
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd Ding_LWE_Key_Exchange-Round1
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd DualModeMS-Round1
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd EMBLEM_R_EMBLEM-Round1
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd Falcon-Round3
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd FrodoKEM-Round3
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd Gravity_SPHINCS-Round1
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd Gui-Round1
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd HQC-Round4
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd KCL-Round1
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd KINDI-Round1
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd Lima-Round1
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd LOTUS-Round1
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd NewHope-Round1
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd NTRUEncrypt-Round1
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd NTRU_HRSS_KEM-Round1
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd NTRU-Prime-Round3
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd NTRU-Round3
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd Odd_Manhattan-Round1
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd Picnic-Round3
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd pqNTRUsign-Round1
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd SABER-Round3
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd SPHINCS-Round3
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd ThreeBears-Round1
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 cd Titanium-Round1
 echo "moved to: ${PWD}"
-./build_all.sh
+./test_all.sh
 cd ..
 
 
