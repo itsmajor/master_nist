968903638315a662976b401ba61d1ee23af1cf6d
Tomas <tomas.itsmajor@gmail.com>
2024-03-17 15:39:15 +0100

    build_all EMBLEM

---
+++ EMBLEM_R_EMBLEM-Round1/build_all.sh
@@ -0,0 +1,9 @@
+cd Optimized_Implementation/EMBLEM_OPT
+echo "moved to: ${PWD}"
+make all
+cd ../..
+
+cd Optimized_Implementation/R_EMBLEM_OPT
+echo "moved to: ${PWD}"
+make all
+cd ../..
---
+++ EMBLEM_R_EMBLEM-Round1/test_all.sh
@@ -0,0 +1,9 @@
+cd Optimized_Implementation/EMBLEM_OPT
+echo "test EMBLEM_OPT"
+./EMBLEM
+cd ../..
+
+cd Optimized_Implementation/R_EMBLEM_OPT
+echo "test R_EMBLEM_OPT"
+./R_EMBLEM_OPT
+cd ../..
