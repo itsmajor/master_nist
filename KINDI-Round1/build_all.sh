4a700cb904e42b9e35b0bf789521fdbddd644b2c
Tomas <tomas.itsmajor@gmail.com>
2024-03-17 19:30:37 +0100

    build_all KINDI

---
+++ KINDI-Round1/build_all.sh
@@ -0,0 +1,49 @@
+cd Reference_Implementation/encrypt/KINDI-256-3-4-2
+echo "moved to: ${PWD}"
+make all
+cd ../../..
+
+cd Reference_Implementation/encrypt/KINDI-256-5-2-2
+echo "moved to: ${PWD}"
+make all
+cd ../../..
+
+cd Reference_Implementation/encrypt/KINDI-512-2-2-2
+echo "moved to: ${PWD}"
+make all
+cd ../../..
+
+cd Reference_Implementation/encrypt/KINDI-512-2-4-1
+echo "moved to: ${PWD}"
+make all
+cd ../../..
+
+cd Reference_Implementation/encrypt/KINDI-512-3-2-1
+echo "moved to: ${PWD}"
+make all
+cd ../../..
+
+cd Reference_Implementation/kem/KINDI-256-3-4-2
+echo "moved to: ${PWD}"
+make all
+cd ../../..
+
+cd Reference_Implementation/kem/KINDI-256-5-2-2
+echo "moved to: ${PWD}"
+make all
+cd ../../..
+
+cd Reference_Implementation/kem/KINDI-512-2-2-2
+echo "moved to: ${PWD}"
+make all
+cd ../../..
+
+cd Reference_Implementation/kem/KINDI-512-2-4-1
+echo "moved to: ${PWD}"
+make all
+cd ../../..
+
+cd Reference_Implementation/kem/KINDI-512-3-2-1
+echo "moved to: ${PWD}"
+make all
+cd ../../..
---
+++ KINDI-Round1/test_all.sh
@@ -0,0 +1,49 @@
+cd Reference_Implementation/encrypt/KINDI-256-3-4-2
+echo "test encrypt KINDI-256-3-4-2"
+./PQCgenKAT_encrypt
+cd ../../..
+
+cd Reference_Implementation/encrypt/KINDI-256-5-2-2
+echo "test encrypt KINDI-256-5-2-2"
+./PQCgenKAT_encrypt
+cd ../../..
+
+cd Reference_Implementation/encrypt/KINDI-512-2-2-2
+echo "test encrypt KINDI-512-2-2-2"
+./PQCgenKAT_encrypt
+cd ../../..
+
+cd Reference_Implementation/encrypt/KINDI-512-2-4-1
+echo "test encrypt KINDI-512-2-4-1"
+./PQCgenKAT_encrypt
+cd ../../..
+
+cd Reference_Implementation/encrypt/KINDI-512-3-2-1
+echo "test encrypt KINDI-512-3-2-1"
+./PQCgenKAT_encrypt
+cd ../../..
+
+cd Reference_Implementation/kem/KINDI-256-3-4-2
+echo "test kem KINDI-256-3-4-2"
+./PQCgenKAT_kem
+cd ../../..
+
+cd Reference_Implementation/kem/KINDI-256-5-2-2
+echo "test kem KINDI-256-5-2-2"
+./PQCgenKAT_kem
+cd ../../..
+
+cd Reference_Implementation/kem/KINDI-512-2-2-2
+echo "test kem KINDI-512-2-2-2"
+./PQCgenKAT_kem
+cd ../../..
+
+cd Reference_Implementation/kem/KINDI-512-2-4-1
+echo "test kem KINDI-512-2-4-1"
+./PQCgenKAT_kem
+cd ../../..
+
+cd Reference_Implementation/kem/KINDI-512-3-2-1
+echo "test kem KINDI-512-3-2-1"
+./PQCgenKAT_kem
+cd ../../..
