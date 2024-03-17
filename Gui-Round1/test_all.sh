930e4d92ea659dba3cc02b32ff01521fff07ace8
Tomas <tomas.itsmajor@gmail.com>
2024-03-17 17:48:16 +0100

    build_all GUI

--- Gui-Round1/test_all.sh
+++ Gui-Round1/test_all.sh
@@ -1,20 +1,23 @@
 cd Optimized_Implementation/amd64/tests/Gui-184
 echo "test Gui-184"
-./gui-genkey
-./gui-sign
-./gui-verify
-cd ..
+#gui-genkey pk_file_name sk_file_name [random_seed_file]
+./gui-genkey pk sk
+#gui-sign sk_file_name file_to_be_signed [random_seed_file]
+./gui-sign sk gui-genkey > sig
+#gui-verify pk_file_name signature_file_name message_file_name
+./gui-verify pk sig gui-genkey
+cd ../../../..
 
-cd Optimized_Implementation/amd64/tests/Gui-184
+cd Optimized_Implementation/amd64/tests/Gui-312
 echo "test Gui-184"
-./gui-genkey
-./gui-sign
-./gui-verify
-cd ..
+./gui-genkey pk sk
+./gui-sign sk gui-genkey > sig
+./gui-verify pk sig gui-genkey
+cd ../../../..
 
-cd Optimized_Implementation/amd64/tests/Gui-184
+cd Optimized_Implementation/amd64/tests/Gui-448
 echo "test Gui-184"
-./gui-genkey
-./gui-sign
-./gui-verify
-cd ..
+./gui-genkey pk sk
+./gui-sign sk gui-genkey > sig
+./gui-verify pk sig gui-genkey
+cd ../../../..
