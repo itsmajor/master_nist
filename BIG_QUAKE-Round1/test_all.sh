70ea4b5142b73889b06a34dcec5070c325a463bd
Tomas <tomas.itsmajor@gmail.com>
2024-03-17 14:32:42 +0100

    rename big quake output file

---
+++ BIG_QUAKE-Round1/test_all.sh
@@ -0,0 +1,14 @@
+cd Optimized_Implementation/BIG_QUAKE_1
+echo "test BIG_QUAKE_1"
+./PQCgenKAT_kem
+cd ../..
+
+cd Optimized_Implementation/BIG_QUAKE_3
+echo "test BIG_QUAKE_3"
+./PQCgenKAT_kem
+cd ../..
+
+cd Optimized_Implementation/BIG_QUAKE_5
+echo "test BIG_QUAKE_5"
+./PQCgenKAT_kem
+cd ../..
