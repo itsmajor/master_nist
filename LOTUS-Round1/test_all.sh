d1815f3b4b53056adcbcb094524cfcc9cb525ecd
Tomas <tomas.itsmajor@gmail.com>
2024-03-18 19:32:39 +0100

    build_all Lotus

--- LOTUS-Round1/test_all.sh
+++ LOTUS-Round1/test_all.sh
@@ -1,29 +1,29 @@
 cd Optimized_Implementation/encrypt/tests/lotus128
 echo "test encrypt lotus128"
 ./PQCgenKAT_encrypt
-cd ../../..
+cd ../../../..
 
 cd Optimized_Implementation/encrypt/tests/lotus192
 echo "test encrypt lotus192"
 ./PQCgenKAT_encrypt
-cd ../../..
+cd ../../../..
 
 cd Optimized_Implementation/encrypt/tests/lotus256
 echo "test encrypt lotus256"
 ./PQCgenKAT_encrypt
-cd ../../..
+cd ../../../..
 
 cd Optimized_Implementation/kem/tests/lotus128
 echo "test kem lotus128"
 ./PQCgenKAT_kem
-cd ../../..
+cd ../../../..
 
 cd Optimized_Implementation/kem/tests/lotus192
 echo "test kem lotus192"
 ./PQCgenKAT_kem
-cd ../../..
+cd ../../../..
 
 cd Optimized_Implementation/kem/tests/lotus256
 echo "test kem lotus256"
 ./PQCgenKAT_kem
-cd ../../..
+cd ../../../..
