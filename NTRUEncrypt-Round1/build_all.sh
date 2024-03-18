e888b9930b69a8ee110b4ef7a543761837faf5c2
Tomas <tomas.itsmajor@gmail.com>
2024-03-18 19:52:35 +0100

    build_all NTRUEncrypt

--- NTRUEncrypt-Round1/build_all.sh
+++ NTRUEncrypt-Round1/build_all.sh
@@ -4,35 +4,35 @@ cd Reference_Implementation/ntru-kem-1024
 echo "moved to: ${PWD}"
 make
 mv ntru-kem-1024 ../tests
-cd ../../..
+cd ../..
 
 cd Reference_Implementation/ntru-kem-443
 echo "moved to: ${PWD}"
 make
 mv ntru-kem-443 ../tests
-cd ../../..
+cd ../..
 
 cd Reference_Implementation/ntru-kem-743
 echo "moved to: ${PWD}"
 make
 mv ntru-kem-743 ../tests
-cd ../../..
+cd ../..
 
 cd Reference_Implementation/ntru-pke-1024
 echo "moved to: ${PWD}"
 make
 mv ntru-pke-1024 ../tests
-cd ../../..
+cd ../..
 
 cd Reference_Implementation/ntru-pke-443
 echo "moved to: ${PWD}"
 make
 mv ntru-pke-443 ../tests
-cd ../../..
+cd ../..
 
 cd Reference_Implementation/ntru-pke-743
 echo "moved to: ${PWD}"
 make
 mv ntru-pke-743 ../tests
-cd ../../..
+cd ../..
 
