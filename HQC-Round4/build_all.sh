899fb80a88b1080b4d2bdb4c884d47bfcf8367bb
Tomas <tomas.itsmajor@gmail.com>
2024-03-24 13:27:47 +0100

    build_all hqc

---
+++ HQC-Round4/build_all.sh
@@ -0,0 +1,22 @@
+mkdir -p tests
+
+cd Additional_Implementation/hqc-128
+echo "moved to: ${PWD}"
+make folders
+make hqc-128
+mv bin/hqc-128 ../../tests/hqc-128
+cd ../..
+
+cd Additional_Implementation/hqc-192
+echo "moved to: ${PWD}"
+make folders
+make hqc-192
+mv bin/hqc-192 ../../tests/hqc-192
+cd ../..
+
+cd Additional_Implementation/hqc-256
+echo "moved to: ${PWD}"
+make folders
+make hqc-256
+mv bin/hqc-256 ../../tests/hqc-256
+cd ../..
---
+++ HQC-Round4/test_all.sh
@@ -0,0 +1,10 @@
+cd tests
+
+echo "test hqc-128"
+./hqc-128
+echo "test hqc-192"
+./hqc-192
+echo "test hqc-256"
+./hqc-256
+
+cd ..
