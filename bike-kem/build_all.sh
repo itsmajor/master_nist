4ac5ceb56cb1ca52cff05a4628de1dc0e91c51bf
Tomas <tomas.itsmajor@gmail.com>
2024-04-04 01:28:14 +0200

    update build_all bike

--- bike-kem/build_all.sh
+++ bike-kem/build_all.sh
@@ -1,25 +1,25 @@
-mkdir -p tests
+mkdir -p tests2
 mkdir -p build
 cd build
 rm -R *
 
 echo "*** modified measurement parameter (REPEAT 10, OUTER_REPEAT 1, WARMUP 0) in include/internal/measurement.h ****"
-echo "cmake bike level 1 (avg of 10 repeats)"
+echo "cmake bike level 1 (10 repeats)"
 cmake -DCMAKE_BUILD_TYPE=Release -DLEVEL=1 -DNUM_OF_TESTS=1 -DRDTSC=1 ..
 make
-mv bike-test ../tests/bike-test-1
+mv bike-test ../tests2/bike-test-1
 rm -R *
 
-echo "cmake bike level 3 (avg of 10 repeats)"
+echo "cmake bike level 3 (10 repeats)"
 cmake -DCMAKE_BUILD_TYPE=Release -DLEVEL=3 -DNUM_OF_TESTS=1 -DRDTSC=1 ..
 make
-mv bike-test ../../master_nist/bike-kem/tests/bike-test-3
+mv bike-test ../tests2/bike-test-3
 rm -R *
 
-echo "cmake bike level 5 (avg of 10 repeats)"
+echo "cmake bike level 5 (10 repeats)"
 cmake -DCMAKE_BUILD_TYPE=Release -DLEVEL=5 -DNUM_OF_TESTS=1 -DRDTSC=1 ..
 make
-mv bike-test ../../master_nist/bike-kem/tests/bike-test-5
+mv bike-test ../tests2/bike-test-5
 rm -R *
 
 echo "build done"
--- bike-kem/test_all.sh
+++ bike-kem/test_all.sh
@@ -1,11 +1,11 @@
-cd tests
-echo "test bike 1"
+cd tests2
+echo "test bike 1 (avg of 10 repeats)"
 ./bike-test-1
 
-echo "test bike 3"
+echo "test bike 3 (avg of 10 repeats)"
 ./bike-test-3
 
-echo "test bike 5"
+echo "test bike 5 (avg of 10 repeats)"
 ./bike-test-5
 
 cd ..
