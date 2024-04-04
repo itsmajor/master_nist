c951f6416bcd5243b0d4cbb523c837e508fb4ab4
Tomas <tomas.itsmajor@gmail.com>
2024-04-04 02:09:04 +0200

    update build_all bike

--- bike-kem/build_all.sh
+++ bike-kem/build_all.sh
@@ -4,22 +4,40 @@ cd build
 rm -R *
 
 echo "*** modified measurement parameter (REPEAT 10, OUTER_REPEAT 1, WARMUP 1) in include/internal/measurement.h ****"
-echo "cmake bike level 1 (10 repeats)"
+echo "cmake bike level 1 SHA3/SHAKE (10 repeats)"
 cmake -DCMAKE_BUILD_TYPE=Release -DLEVEL=1 -DNUM_OF_TESTS=1 -DRDTSC=1 ..
 make
-mv bike-test ../tests2/bike-test-1
+mv bike-test ../tests2/bike-test-1_sha3
 rm -R *
 
-echo "cmake bike level 3 (10 repeats)"
-cmake -DCMAKE_BUILD_TYPE=Release -DLEVEL=3 -DNUM_OF_TESTS=1 -DRDTSC=1 ..
+echo "cmake bike level 3 SHA3/SHAKE (10 repeats)"
+cmake -DCMAKE_BUILD_TYPE=Release -DLEVEL=3  ..
 make
-mv bike-test ../tests2/bike-test-3
+mv bike-test ../tests2/bike-test-3_sha3
 rm -R *
 
-echo "cmake bike level 5 (10 repeats)"
-cmake -DCMAKE_BUILD_TYPE=Release -DLEVEL=5 -DNUM_OF_TESTS=1 -DRDTSC=1 ..
+echo "cmake bike level 5 SHA3/SHAKE (10 repeats)"
+cmake -DCMAKE_BUILD_TYPE=Release -DLEVEL=5 -DRDTSC=1 ..
 make
-mv bike-test ../tests2/bike-test-5
+mv bike-test ../tests2/bike-test-5_sha3
+rm -R *
+
+echo "cmake bike level 1 AES/SHA2 (10 repeats)"
+cmake -DCMAKE_BUILD_TYPE=Release -DLEVEL=1 -DRDTSC=1 -DUSE_AES_AND_SHA2 ..
+make
+mv bike-test ../tests2/bike-test-1_aes
+rm -R *
+
+echo "cmake bike level 3 AES/SHA2 (10 repeats)"
+cmake -DCMAKE_BUILD_TYPE=Release -DLEVEL=3 -DRDTSC=1 -DUSE_AES_AND_SHA2 ..
+make
+mv bike-test ../tests2/bike-test-3_aes
+rm -R *
+
+echo "cmake bike level 5 AES/SHA2 (10 repeats)"
+cmake -DCMAKE_BUILD_TYPE=Release -DLEVEL=5 -DRDTSC=1 -DUSE_AES_AND_SHA2 ..
+make
+mv bike-test ../tests2/bike-test-5_aes
 rm -R *
 
 echo "build done"
--- bike-kem/test_all.sh
+++ bike-kem/test_all.sh
@@ -1,12 +1,21 @@
 cd tests2
-echo "test bike 1 (avg of 10 repeats)"
-./bike-test-1
+echo "test bike 1 SHA3/SHAKE (avg of 10 repeats)"
+./bike-test-1_sha3
 
-echo "test bike 3 (avg of 10 repeats)"
-./bike-test-3
+echo "test bike 1 AES/SHA2 (avg of 10 repeats)"
+./bike-test-1_aes
 
-echo "test bike 5 (avg of 10 repeats)"
-./bike-test-5
+echo "test bike 3 SHA3/SHAKE (avg of 10 repeats)"
+./bike-test-3_sha3
+
+echo "test bike 3 AES/SHA2 (avg of 10 repeats)"
+./bike-test-3_aes
+
+echo "test bike 5 SHA3/SHAKE (avg of 10 repeats)"
+./bike-test-5_sha3
+
+echo "test bike 5 AES/SHA2 (avg of 10 repeats)"
+./bike-test-5_aes
 
 cd ..
 
