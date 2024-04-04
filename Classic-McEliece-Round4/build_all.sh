802c3f08d26c1031f649160d752a460cb428a9ea
Tomas <tomas.itsmajor@gmail.com>
2024-04-04 22:12:25 +0200

    build_all mceliece

--- Classic-McEliece-Round4/build_all.sh
+++ Classic-McEliece-Round4/build_all.sh
@@ -3,59 +3,69 @@ mkdir -p tests
 cd Additional_Implementations/kem/mceliece348864/vec
 echo "moved to: ${PWD}"
 ./build
-#mv PQCgenKAT_kem ../../../tests/PQCgenKAT_kem_newhope512cca
+mkdir -p ../../../../tests/mceliece348864/
+mv run ../../../../tests/mceliece348864/run
 cd ../../../..
 
 cd Additional_Implementations/kem/mceliece348864f/vec
 echo "moved to: ${PWD}"
 ./build
-#mv PQCgenKAT_kem ../../../tests/PQCgenKAT_kem_newhope512cca
+mkdir -p ../../../../tests/mceliece348864f/
+mv run ../../../../tests/mceliece348864f/run
 cd ../../../..
 
 cd Additional_Implementations/kem/mceliece460896/vec
 echo "moved to: ${PWD}"
 ./build
-#mv PQCgenKAT_kem ../../../tests/PQCgenKAT_kem_newhope512cca
+mkdir -p ../../../../tests/mceliece460896/
+mv run ../../../../tests/mceliece460896/run
 cd ../../../..
 
 cd Additional_Implementations/kem/mceliece460896f/vec
 echo "moved to: ${PWD}"
 ./build
-#mv PQCgenKAT_kem ../../../tests/PQCgenKAT_kem_newhope512cca
+mkdir -p ../../../../tests/mceliece460896f/
+mv run ../../../../tests/mceliece460896f/run
 cd ../../../..
 
 cd Additional_Implementations/kem/mceliece6688128/vec
 echo "moved to: ${PWD}"
 ./build
-#mv PQCgenKAT_kem ../../../tests/PQCgenKAT_kem_newhope512cca
+mkdir -p ../../../../tests/mceliece6688128/
+mv run ../../../../tests/mceliece6688128/run
 cd ../../../..
 
 cd Additional_Implementations/kem/mceliece6688128f/vec
 echo "moved to: ${PWD}"
 ./build
-#mv PQCgenKAT_kem ../../../tests/PQCgenKAT_kem_newhope512cca
+mkdir -p ../../../../tests/mceliece6688128f/
+mv run ../../../../tests/mceliece6688128f/run
 cd ../../../..
 
 cd Additional_Implementations/kem/mceliece6960119/vec
 echo "moved to: ${PWD}"
 ./build
-#mv PQCgenKAT_kem ../../../tests/PQCgenKAT_kem_newhope512cca
+mkdir -p ../../../../tests/mceliece6960119/
+mv run ../../../../tests/mceliece6960119/run
 cd ../../../..
 
 cd Additional_Implementations/kem/mceliece6960119f/vec
 echo "moved to: ${PWD}"
 ./build
-#mv PQCgenKAT_kem ../../../tests/PQCgenKAT_kem_newhope512cca
+mkdir -p ../../../../tests/mceliece6960119f/
+mv run ../../../../tests/mceliece6960119f/run
 cd ../../../..
 
 cd Additional_Implementations/kem/mceliece8192128/vec
 echo "moved to: ${PWD}"
 ./build
-#mv PQCgenKAT_kem ../../../tests/PQCgenKAT_kem_newhope512cca
+mkdir -p ../../../../tests/mceliece8192128/
+mv run ../../../../tests/mceliece8192128/run
 cd ../../../..
 
 cd Additional_Implementations/kem/mceliece8192128f/vec
 echo "moved to: ${PWD}"
 ./build
-#mv PQCgenKAT_kem ../../../tests/PQCgenKAT_kem_newhope512cca
+mkdir -p ../../../../tests/mceliece8192128f/
+mv run ../../../../tests/mceliece8192128f/run
 cd ../../../..
--- Classic-McEliece-Round4/test_all.sh
+++ Classic-McEliece-Round4/test_all.sh
@@ -1,49 +1,49 @@
-cd Additional_Implementations/kem/mceliece348864/vec
+cd tests/mceliece348864
 echo "test mceliece348864"
 ./run
-cd ../../../..
+cd ../..
 
-cd Additional_Implementations/kem/mceliece348864f/vec
+cd tests/mceliece348864f
 echo "test mceliece348864f"
 ./run
-cd ../../../..
+cd ../..
 
-cd Additional_Implementations/kem/mceliece460896/vec
+cd tests/mceliece460896
 echo "test mceliece460896"
 ./run
-cd ../../../..
+cd ../..
 
-cd Additional_Implementations/kem/mceliece460896f/vec
+cd tests/mceliece460896f
 echo "test mceliece460896f"
 ./run
-cd ../../../..
+cd ../..
 
-cd Additional_Implementations/kem/mceliece6688128/vec
+cd tests/mceliece6688128
 echo "test mceliece6688128"
 ./run
-cd ../../../..
+cd ../..
 
-cd Additional_Implementations/kem/mceliece6688128f/vec
+cd tests/mceliece6688128f
 echo "test mceliece6688128f"
 ./run
-cd ../../../..
+cd ../..
 
-cd Additional_Implementations/kem/mceliece6960119/vec
+cd tests/mceliece6960119
 echo "test mceliece6960119"
 ./run
-cd ../../../..
+cd ../..
 
-cd Additional_Implementations/kem/mceliece6960119f/vec
+cd tests/mceliece6960119f
 echo "test mceliece6960119f"
 ./run
-cd ../../../..
+cd ../..
 
-cd Additional_Implementations/kem/mceliece8192128/vec
+cd tests/mceliece8192128
 echo "test mceliece8192128"
 ./run
-cd ../../../..
+cd ../..
 
-cd Additional_Implementations/kem/mceliece8192128f/vec
+cd tests/mceliece8192128f
 echo "test mceliece8192128f"
 ./run
-cd ../../../..
+cd ../..
