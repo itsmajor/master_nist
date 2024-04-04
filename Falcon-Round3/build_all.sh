ba68bf4b5b1078ba539e8bbf2c748b4a8afd5c69
Tomas <tomas.itsmajor@gmail.com>
2024-04-05 00:24:45 +0200

    build_all falcon

--- Falcon-Round3/build_all.sh
+++ Falcon-Round3/build_all.sh
@@ -3,13 +3,13 @@ mkdir -p tests
 cd Optimized_Implementation/falcon512/falcon512fpu
 echo "moved to: ${PWD}"
 make
-mv kat512fpu ../../../tests/
+mv build/kat512fpu ../../../tests/
 cd ../../..
 
 cd Optimized_Implementation/falcon1024/falcon1024fpu
 echo "moved to: ${PWD}"
 make
-mv kat1024fpu ../../../tests/
+mv build/kat1024fpu ../../../tests/
 cd ../../..
 
 cd Extra/c
