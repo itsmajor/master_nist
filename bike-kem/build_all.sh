0093d4ba64bf53b58b221af8a6513a6b978fca35
Tomas <tomas.itsmajor@gmail.com>
2024-04-04 02:11:36 +0200

    update build_all bike

--- bike-kem/build_all.sh
+++ bike-kem/build_all.sh
@@ -23,19 +23,19 @@ mv bike-test ../tests2/bike-test-5_sha3
 rm -R *
 
 echo "cmake bike level 1 AES/SHA2 (10 repeats)"
-cmake -DCMAKE_BUILD_TYPE=Release -DLEVEL=1 -DRDTSC=1 -DUSE_AES_AND_SHA2 ..
+cmake -DCMAKE_BUILD_TYPE=Release -DLEVEL=1 -DRDTSC=1 -DUSE_AES_AND_SHA2=1 ..
 make
 mv bike-test ../tests2/bike-test-1_aes
 rm -R *
 
 echo "cmake bike level 3 AES/SHA2 (10 repeats)"
-cmake -DCMAKE_BUILD_TYPE=Release -DLEVEL=3 -DRDTSC=1 -DUSE_AES_AND_SHA2 ..
+cmake -DCMAKE_BUILD_TYPE=Release -DLEVEL=3 -DRDTSC=1 -DUSE_AES_AND_SHA2=1 ..
 make
 mv bike-test ../tests2/bike-test-3_aes
 rm -R *
 
 echo "cmake bike level 5 AES/SHA2 (10 repeats)"
-cmake -DCMAKE_BUILD_TYPE=Release -DLEVEL=5 -DRDTSC=1 -DUSE_AES_AND_SHA2 ..
+cmake -DCMAKE_BUILD_TYPE=Release -DLEVEL=5 -DRDTSC=1 -DUSE_AES_AND_SHA2=1 ..
 make
 mv bike-test ../tests2/bike-test-5_aes
 rm -R *
