be05b4a26915535672aec05b43a1cadf379e39d5
Tomas <tomas.itsmajor@gmail.com>
2024-04-04 23:17:13 +0200

    build_all mceliece

--- Classic-McEliece-Round4/build_all.sh
+++ Classic-McEliece-Round4/build_all.sh
@@ -4,6 +4,7 @@ cd Additional_Implementations/kem/mceliece348864/vec
 echo "moved to: ${PWD}"
 ./build
 mkdir -p ../../../../tests/mceliece348864/
+mv kat ../../../../tests/mceliece348864/kat
 mv run ../../../../tests/mceliece348864/run
 cd ../../../..
 
@@ -11,6 +12,7 @@ cd Additional_Implementations/kem/mceliece348864f/vec
 echo "moved to: ${PWD}"
 ./build
 mkdir -p ../../../../tests/mceliece348864f/
+mv kat ../../../../tests/mceliece348864f/kat
 mv run ../../../../tests/mceliece348864f/run
 cd ../../../..
 
@@ -18,6 +20,7 @@ cd Additional_Implementations/kem/mceliece460896/vec
 echo "moved to: ${PWD}"
 ./build
 mkdir -p ../../../../tests/mceliece460896/
+mv kat ../../../../tests/mceliece460896/kat
 mv run ../../../../tests/mceliece460896/run
 cd ../../../..
 
@@ -25,6 +28,7 @@ cd Additional_Implementations/kem/mceliece460896f/vec
 echo "moved to: ${PWD}"
 ./build
 mkdir -p ../../../../tests/mceliece460896f/
+mv kat ../../../../tests/mceliece460896f/kat
 mv run ../../../../tests/mceliece460896f/run
 cd ../../../..
 
@@ -32,6 +36,7 @@ cd Additional_Implementations/kem/mceliece6688128/vec
 echo "moved to: ${PWD}"
 ./build
 mkdir -p ../../../../tests/mceliece6688128/
+mv kat ../../../../tests/mceliece6688128/kat
 mv run ../../../../tests/mceliece6688128/run
 cd ../../../..
 
@@ -39,6 +44,7 @@ cd Additional_Implementations/kem/mceliece6688128f/vec
 echo "moved to: ${PWD}"
 ./build
 mkdir -p ../../../../tests/mceliece6688128f/
+mv kat ../../../../tests/mceliece6688128f/kat
 mv run ../../../../tests/mceliece6688128f/run
 cd ../../../..
 
@@ -46,6 +52,7 @@ cd Additional_Implementations/kem/mceliece6960119/vec
 echo "moved to: ${PWD}"
 ./build
 mkdir -p ../../../../tests/mceliece6960119/
+mv kat ../../../../tests/mceliece6960119/kat
 mv run ../../../../tests/mceliece6960119/run
 cd ../../../..
 
@@ -53,6 +60,7 @@ cd Additional_Implementations/kem/mceliece6960119f/vec
 echo "moved to: ${PWD}"
 ./build
 mkdir -p ../../../../tests/mceliece6960119f/
+mv kat ../../../../tests/mceliece6960119f/kat
 mv run ../../../../tests/mceliece6960119f/run
 cd ../../../..
 
@@ -60,6 +68,7 @@ cd Additional_Implementations/kem/mceliece8192128/vec
 echo "moved to: ${PWD}"
 ./build
 mkdir -p ../../../../tests/mceliece8192128/
+mv kat ../../../../tests/mceliece8192128/kat
 mv run ../../../../tests/mceliece8192128/run
 cd ../../../..
 
@@ -67,5 +76,6 @@ cd Additional_Implementations/kem/mceliece8192128f/vec
 echo "moved to: ${PWD}"
 ./build
 mkdir -p ../../../../tests/mceliece8192128f/
+mv kat ../../../../tests/mceliece8192128f/kat
 mv run ../../../../tests/mceliece8192128f/run
 cd ../../../..
