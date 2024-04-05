f383115b301c3803b6ee725b941e1338c470c522
Tomas <tomas.itsmajor@gmail.com>
2024-04-05 13:43:34 +0200

    build_all NTRU

---
+++ NTRU-Round3/test_all.sh
@@ -0,0 +1,23 @@
+cd tests
+
+cd ntruhrss701
+echo "generate kat NTRU ntruhrss701"
+./PQCgenKAT_kem
+cd ..
+
+cd ntruhps2048509
+echo "generate kat NTRU ntruhps2048509"
+./PQCgenKAT_kem
+cd ..
+
+cd ntruhps2048677
+echo "generate kat NTRU ntruhps2048677"
+./PQCgenKAT_kem
+cd ..
+
+cd ntruhps4096821
+echo "generate kat NTRU ntruhps4096821"
+./PQCgenKAT_kem
+cd ..
+
+cd ..
