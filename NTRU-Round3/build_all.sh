7165769d4479df5f06ee284f9ebb7d1707cf2ddc
Tomas <tomas.itsmajor@gmail.com>
2024-04-05 13:45:19 +0200

    build_all NTRU

--- NTRU-Round3/build_all.sh
+++ NTRU-Round3/build_all.sh
@@ -5,25 +5,25 @@ mkdir -p tests/ntruhps2048677
 mkdir -p tests/ntruhps4096821
 
 
-cd /Optimized_Implementation/crypto_kem/ntruhrss701
+cd Optimized_Implementation/crypto_kem/ntruhrss701
 echo "moved to: ${PWD}"
 make
 mv PQCgenKAT_kem ../../../tests/ntruhrss701/
 cd ../../..
 
-cd /Optimized_Implementation/crypto_kem/ntruhps2048509
+cd Optimized_Implementation/crypto_kem/ntruhps2048509
 echo "moved to: ${PWD}"
 make
 mv PQCgenKAT_kem ../../../tests/ntruhps2048509/
 cd ../../..
 
-cd /Optimized_Implementation/crypto_kem/ntruhps2048677
+cd Optimized_Implementation/crypto_kem/ntruhps2048677
 echo "moved to: ${PWD}"
 make
 mv PQCgenKAT_kem ../../../tests/ntruhps2048677/
 cd ../../..
 
-cd /Optimized_Implementation/crypto_kem/ntruhps4096821
+cd Optimized_Implementation/crypto_kem/ntruhps4096821
 echo "moved to: ${PWD}"
 make
 mv PQCgenKAT_kem ../../../tests/ntruhps4096821/
