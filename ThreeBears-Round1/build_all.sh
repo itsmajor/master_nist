f14a3c64d58b5b5a1136938a6209b334c76fc49b
Tomas <tomas.itsmajor@gmail.com>
2024-04-07 18:04:10 +0200

    threebears build_all

--- ThreeBears-Round1/build_all.sh
+++ ThreeBears-Round1/build_all.sh
@@ -8,37 +8,37 @@ mkdir -p tests/OI/PapaBearEphem
 cd Optimized_Implementation/crypto_kem/BabyBear
 echo "moved to: ${PWD}"
 make
-cp genkat ../../../tests/OI/BabyBear
+cp build/genkat ../../../tests/OI/BabyBear
 cd ../../..
 
 cd Optimized_Implementation/crypto_kem/BabyBearEphem
 echo "moved to: ${PWD}"
 make
-cp genkat ../../../tests/OI/BabyBearEphem
+cp build/genkat ../../../tests/OI/BabyBearEphem
 cd ../../..
 
 cd Optimized_Implementation/crypto_kem/MamaBear
 echo "moved to: ${PWD}"
 make
-cp genkat ../../../tests/OI/MamaBear
+cp build/genkat ../../../tests/OI/MamaBear
 cd ../../..
 
 cd Optimized_Implementation/crypto_kem/MamaBearEphem
 echo "moved to: ${PWD}"
 make
-cp genkat ../../../tests/OI/MamaBearEphem
+cp build/genkat ../../../tests/OI/MamaBearEphem
 cd ../../..
 
 cd Optimized_Implementation/crypto_kem/PapaBear
 echo "moved to: ${PWD}"
 make
-cp genkat ../../../tests/OI/PapaBear
+cp build/genkat ../../../tests/OI/PapaBear
 cd ../../..
 
 cd Optimized_Implementation/crypto_kem/PapaBearEphem
 echo "moved to: ${PWD}"
 make
-cp genkat ../../../tests/OI/PapaBearEphem
+cp build/genkat ../../../tests/OI/PapaBearEphem
 cd ../../..
 
 
@@ -53,37 +53,37 @@ mkdir -p tests/AI_LowMem/PapaBearEphem
 cd Additional_Implementations/Low_Memory/crypto_kem/BabyBear
 echo "moved to: ${PWD}"
 make
-cp genkat ../../../../tests/AI_LowMem/BabyBear
+cp build/genkat ../../../../tests/AI_LowMem/BabyBear
 cd ../../../..
 
 cd Additional_Implementations/Low_Memory/crypto_kem/BabyBearEphem
 echo "moved to: ${PWD}"
 make
-cp genkat ../../../../tests/AI_LowMem/BabyBearEphem
+cp build/genkat ../../../../tests/AI_LowMem/BabyBearEphem
 cd ../../../..
 
 cd Additional_Implementations/Low_Memory/crypto_kem/MamaBear
 echo "moved to: ${PWD}"
 make
-cp genkat ../../../../tests/AI_LowMem/MamaBear
+cp build/genkat ../../../../tests/AI_LowMem/MamaBear
 cd ../../../..
 
 cd Additional_Implementations/Low_Memory/crypto_kem/MamaBearEphem
 echo "moved to: ${PWD}"
 make
-cp genkat ../../../../tests/AI_LowMem/MamaBearEphem
+cp build/genkat ../../../../tests/AI_LowMem/MamaBearEphem
 cd ../../../..
 
 cd Additional_Implementations/Low_Memory/crypto_kem/PapaBear
 echo "moved to: ${PWD}"
 make
-cp genkat ../../../../tests/AI_LowMem/PapaBear
+cp build/genkat ../../../../tests/AI_LowMem/PapaBear
 cd ../../../..
 
 cd Additional_Implementations/Low_Memory/crypto_kem/PapaBearEphem
 echo "moved to: ${PWD}"
 make
-cp genkat ../../../../tests/AI_LowMem/PapaBearEphem
+cp build/genkat ../../../../tests/AI_LowMem/PapaBearEphem
 cd ../../../..
 
 
@@ -98,35 +98,35 @@ mkdir -p tests/AI_Asm/PapaBearEphem
 cd Additional_Implementations/Low_Memory/crypto_kem/BabyBear
 echo "moved to: ${PWD}"
 make
-cp genkat ../../../../tests/AI_Asm/BabyBear
+cp build/genkat ../../../../tests/AI_Asm/BabyBear
 cd ../../../..
 
 cd Additional_Implementations/Low_Memory/crypto_kem/BabyBearEphem
 echo "moved to: ${PWD}"
 make
-cp genkat ../../../../tests/AI_Asm/BabyBearEphem
+cp build/genkat ../../../../tests/AI_Asm/BabyBearEphem
 cd ../../../..
 
 cd Additional_Implementations/Low_Memory/crypto_kem/MamaBear
 echo "moved to: ${PWD}"
 make
-cp genkat ../../../../tests/AI_Asm/MamaBear
+cp build/genkat ../../../../tests/AI_Asm/MamaBear
 cd ../../../..
 
 cd Additional_Implementations/Low_Memory/crypto_kem/MamaBearEphem
 echo "moved to: ${PWD}"
 make
-cp genkat ../../../../tests/AI_Asm/MamaBearEphem
+cp build/genkat ../../../../tests/AI_Asm/MamaBearEphem
 cd ../../../..
 
 cd Additional_Implementations/Low_Memory/crypto_kem/PapaBear
 echo "moved to: ${PWD}"
 make
-cp genkat ../../../../tests/AI_Asm/PapaBear
+cp build/genkat ../../../../tests/AI_Asm/PapaBear
 cd ../../../..
 
 cd Additional_Implementations/Low_Memory/crypto_kem/PapaBearEphem
 echo "moved to: ${PWD}"
 make
-cp genkat ../../../../tests/AI_Asm/PapaBearEphem
+cp build/genkat ../../../../tests/AI_Asm/PapaBearEphem
 cd ../../../..
