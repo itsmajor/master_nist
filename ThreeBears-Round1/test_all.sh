441bd4129814c64f118a9a9ccac5b9344784b622
Tomas <tomas.itsmajor@gmail.com>
2024-04-13 18:13:15 +0200

    threebears build_all

---
+++ ThreeBears-Round1/test_all.sh
@@ -0,0 +1,91 @@
+cd tests
+
+cd OI/BabyBear
+echo "create KAT OI BabyBear"
+./genkat
+cd ../..
+
+cd OI/BabyBearEphem
+echo "create KAT OI BabyBearEphem"
+./genkat
+cd ../..
+
+cd OI/MamaBear
+echo "create KAT OI MamaBear"
+./genkat
+cd ../..
+
+cd OI/MamaBearEphem
+echo "create KAT OI MamaBearEphem"
+./genkat
+cd ../..
+
+cd OI/PapaBear
+echo "create KAT OI PapaBear"
+./genkat
+cd ../..
+
+cd OI/PapaBearEphem
+echo "create KAT OI PapaBearEphem"
+./genkat
+cd ../..
+
+cd AI_LowMem/BabyBear
+echo "create KAT AI_LowMem BabyBear"
+./genkat
+cd ../..
+
+cd AI_LowMem/BabyBearEphem
+echo "create KAT AI_LowMem BabyBearEphem"
+./genkat
+cd ../..
+
+cd AI_LowMem/MamaBear
+echo "create KAT AI_LowMem MamaBear"
+./genkat
+cd ../..
+
+cd AI_LowMem/MamaBearEphem
+echo "create KAT AI_LowMem MamaBearEphem"
+./genkat
+cd ../..
+
+cd AI_LowMem/PapaBear
+echo "create KAT AI_LowMem PapaBear"
+./genkat
+cd ../..
+
+cd AI_LowMem/PapaBearEphem
+echo "create KAT AI_LowMem PapaBearEphem"
+./genkat
+cd ../..
+
+cd AI_Asm/BabyBear
+echo "create KAT AI_Asm BabyBear"
+./genkat
+cd ../..
+
+cd AI_Asm/BabyBearEphem
+echo "create KAT AI_Asm BabyBearEphem"
+./genkat
+cd ../..
+
+cd AI_Asm/MamaBear
+echo "create KAT AI_Asm MamaBear"
+./genkat
+cd ../..
+
+cd AI_Asm/MamaBearEphem
+echo "create KAT AI_Asm MamaBearEphem"
+./genkat
+cd ../..
+
+cd AI_Asm/PapaBear
+echo "create KAT AI_Asm PapaBear"
+./genkat
+cd ../..
+
+cd AI_Asm/PapaBearEphem
+echo "create KAT AI_Asm PapaBearEphem"
+./genkat
+cd ../..
