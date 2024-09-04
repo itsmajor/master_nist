958241f0eead7e1536c3de1b8ec476f475a7f1b6
Tomas <tomas.itsmajor@gmail.com>
2024-09-04 22:02:07 +0200

    valgrind BIG_QUAKE

--- BIG_QUAKE-Round1/test_all.sh
+++ BIG_QUAKE-Round1/test_all.sh
@@ -1,76 +1,108 @@
-mkdir -p testresult
-mkdir -p testresult/BIG_QUAKE_1
-mkdir -p testresult/BIG_QUAKE_3
-mkdir -p testresult/BIG_QUAKE_5
-rm testresult/BIG_QUAKE_1/* 2> /dev/null
-rm testresult/BIG_QUAKE_3/* 2> /dev/null
-rm testresult/BIG_QUAKE_5/* 2> /dev/null
+doValgrindFull=$1;
+doValgrindKeygen=$2;
+doValgrindEncDec=$3;
+
+if [ ! $# -eq 3 ];
+then
+  echo "3 parameter expected for valgrind control (doValgrindFull, doValgrindKeygen, doValgrindEncDec)"
+  echo "set 1 to be executed, 0 to skip it"
+  echo "example: $ ./test_all.sh 0 1 0   --> will do only valgrind keygen"
+  echo "example: $ ./test_all.sh 0 1 1   --> will do valgrind keygen, encoding and decoding"
+  echo "example: $ ./test_all.sh 0 0 0   --> no valgrind"
+  exit
+fi
+
+mkdir -p ../testresult
+mkdir -p ../testresult/BIG_QUAKE_1
+mkdir -p ../testresult/BIG_QUAKE_3
+mkdir -p ../testresult/BIG_QUAKE_5
+rm ../testresult/BIG_QUAKE_1/* 2> /dev/null
+rm ../testresult/BIG_QUAKE_3/* 2> /dev/null
+rm ../testresult/BIG_QUAKE_5/* 2> /dev/null
 
 cd Optimized_Implementation/BIG_QUAKE_1
 echo "test BIG_QUAKE_1"
-date +'%d.%m.%Y %H:%M:%S.%3N'
-#valgrind -q --tool=massif --massif-out-file=massif.out.full.heap --heap=yes --stacks=no ./PQCgenKAT_kem
-#valgrind -q --tool=massif --massif-out-file=massif.out.full.stack --heap=no --stacks=yes ./PQCgenKAT_kem
-#echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind full done
-#valgrind -q --tool=massif --massif-out-file=massif.out.keygen.heap --heap=yes --stacks=no ./PQCgenKAT_kem_keygen
-#valgrind -q --tool=massif --massif-out-file=massif.out.keygen.stack --heap=no --stacks=yes ./PQCgenKAT_kem_keygen
-#echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind keygen done
+echo $(date +'%d.%m.%Y %H:%M:%S.%3N') - start
 ./PQCgenKAT_kem
-cp PQCkemKAT.req ../../testresult/BIG_QUAKE_1/
-cp PQCkemKAT*.rsp ../../testresult/BIG_QUAKE_1/
 echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - PQCgenKAT_kem done
-valgrind -q --tool=massif --massif-out-file=massif.out.enc.heap --heap=yes --stacks=no ./PQCgenKAT_kem_enc
-valgrind -q --tool=massif --massif-out-file=massif.out.enc.stack --heap=no --stacks=yes ./PQCgenKAT_kem_enc
-echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind enc done
-valgrind -q --tool=massif --massif-out-file=massif.out.dec.heap --heap=yes --stacks=no ./PQCgenKAT_kem_dec
-valgrind -q --tool=massif --massif-out-file=massif.out.dec.stack --heap=no --stacks=yes ./PQCgenKAT_kem_dec
-echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind dec done
-mv massif.* ../../testresult/BIG_QUAKE_1/
+if [ $doValgrindFull == "1" ]; then
+  valgrind -q --tool=massif --massif-out-file=massif.out.full.heap --heap=yes --stacks=no ./PQCgenKAT_kem
+  valgrind -q --tool=massif --massif-out-file=massif.out.full.stack --heap=no --stacks=yes ./PQCgenKAT_kem
+  echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind full done
+fi
+if [ $doValgrindKeygen == "1" ]; then
+  valgrind -q --tool=massif --massif-out-file=massif.out.keygen.heap --heap=yes --stacks=no ./PQCgenKAT_kem_keygen
+  valgrind -q --tool=massif --massif-out-file=massif.out.keygen.stack --heap=no --stacks=yes ./PQCgenKAT_kem_keygen
+  echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind keygen done
+fi
+if [ $doValgrindEncDec == "1" ]; then
+  valgrind -q --tool=massif --massif-out-file=massif.out.enc.heap --heap=yes --stacks=no ./PQCgenKAT_kem_enc
+  valgrind -q --tool=massif --massif-out-file=massif.out.enc.stack --heap=no --stacks=yes ./PQCgenKAT_kem_enc
+  echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind enc done
+  valgrind -q --tool=massif --massif-out-file=massif.out.dec.heap --heap=yes --stacks=no ./PQCgenKAT_kem_dec
+  valgrind -q --tool=massif --massif-out-file=massif.out.dec.stack --heap=no --stacks=yes ./PQCgenKAT_kem_dec
+  echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind dec done
+fi
+mv PQCkemKAT.req ../../../testresult/BIG_QUAKE_1/
+mv PQCkemKAT*.rsp ../../../testresult/BIG_QUAKE_1/
+mv massif.* ../../../testresult/BIG_QUAKE_1/ 2> /dev/null
 cd ../..
 
 cd Optimized_Implementation/BIG_QUAKE_3
 echo "test BIG_QUAKE_3"
 date +'%d.%m.%Y %H:%M:%S.%3N'
-#valgrind -q --tool=massif --massif-out-file=massif.out.full.heap --heap=yes --stacks=no ./PQCgenKAT_kem
-#valgrind -q --tool=massif --massif-out-file=massif.out.full.stack --heap=no --stacks=yes ./PQCgenKAT_kem
-#echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind full done
-#valgrind -q --tool=massif --massif-out-file=massif.out.keygen.heap --heap=yes --stacks=no ./PQCgenKAT_kem_keygen
-#valgrind -q --tool=massif --massif-out-file=massif.out.keygen.stack --heap=no --stacks=yes ./PQCgenKAT_kem_keygen
-#echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind keygen done
 ./PQCgenKAT_kem
-cp PQCkemKAT.req ../../testresult/BIG_QUAKE_3/
-cp PQCkemKAT*.rsp ../../testresult/BIG_QUAKE_3/
 echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - PQCgenKAT_kem done
-valgrind -q --tool=massif --massif-out-file=massif.out.enc.heap --heap=yes --stacks=no ./PQCgenKAT_kem_enc
-valgrind -q --tool=massif --massif-out-file=massif.out.enc.stack --heap=no --stacks=yes ./PQCgenKAT_kem_enc
-echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind enc done
-valgrind -q --tool=massif --massif-out-file=massif.out.dec.heap --heap=yes --stacks=no ./PQCgenKAT_kem_dec
-valgrind -q --tool=massif --massif-out-file=massif.out.dec.stack --heap=no --stacks=yes ./PQCgenKAT_kem_dec
-echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind dec done
-mv massif.* ../../testresult/BIG_QUAKE_3/
+if [ $doValgrindFull == "1" ]; then
+  valgrind -q --tool=massif --massif-out-file=massif.out.full.heap --heap=yes --stacks=no ./PQCgenKAT_kem
+  valgrind -q --tool=massif --massif-out-file=massif.out.full.stack --heap=no --stacks=yes ./PQCgenKAT_kem
+  echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind full done
+fi
+if [ $doValgrindKeygen == "1" ]; then
+  valgrind -q --tool=massif --massif-out-file=massif.out.keygen.heap --heap=yes --stacks=no ./PQCgenKAT_kem_keygen
+  valgrind -q --tool=massif --massif-out-file=massif.out.keygen.stack --heap=no --stacks=yes ./PQCgenKAT_kem_keygen
+  echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind keygen done
+fi
+if [ $doValgrindEncDec == "1" ]; then
+  valgrind -q --tool=massif --massif-out-file=massif.out.enc.heap --heap=yes --stacks=no ./PQCgenKAT_kem_enc
+  valgrind -q --tool=massif --massif-out-file=massif.out.enc.stack --heap=no --stacks=yes ./PQCgenKAT_kem_enc
+  echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind enc done
+  valgrind -q --tool=massif --massif-out-file=massif.out.dec.heap --heap=yes --stacks=no ./PQCgenKAT_kem_dec
+  valgrind -q --tool=massif --massif-out-file=massif.out.dec.stack --heap=no --stacks=yes ./PQCgenKAT_kem_dec
+  echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind dec done
+fi
+mv PQCkemKAT.req ../../../testresult/BIG_QUAKE_3/
+mv PQCkemKAT*.rsp ../../../testresult/BIG_QUAKE_3/
+mv massif.* ../../../testresult/BIG_QUAKE_3/ 2> /dev/null
 cd ../..
 
 cd Optimized_Implementation/BIG_QUAKE_5
 echo "test BIG_QUAKE_5"
 date +'%d.%m.%Y %H:%M:%S.%3N'
-#valgrind -q --tool=massif --massif-out-file=massif.out.full.heap --heap=yes --stacks=no ./PQCgenKAT_kem
-#valgrind -q --tool=massif --massif-out-file=massif.out.full.stack --heap=no --stacks=yes ./PQCgenKAT_kem
-#echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind full done
-#valgrind -q --tool=massif --massif-out-file=massif.out.keygen.heap --heap=yes --stacks=no ./PQCgenKAT_kem_keygen
-#valgrind -q --tool=massif --massif-out-file=massif.out.keygen.stack --heap=no --stacks=yes ./PQCgenKAT_kem_keygen
-#echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind keygen done
 ./PQCgenKAT_kem
-cp PQCkemKAT.req ../../testresult/BIG_QUAKE_5/
-cp PQCkemKAT*.rsp ../../testresult/BIG_QUAKE_5/
 echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - PQCgenKAT_kem done
-valgrind -q --tool=massif --massif-out-file=massif.out.enc.heap --heap=yes --stacks=no ./PQCgenKAT_kem_enc
-valgrind -q --tool=massif --massif-out-file=massif.out.enc.stack --heap=no --stacks=yes ./PQCgenKAT_kem_enc
-echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind enc done
-valgrind -q --tool=massif --massif-out-file=massif.out.dec.heap --heap=yes --stacks=no ./PQCgenKAT_kem_dec
-valgrind -q --tool=massif --massif-out-file=massif.out.dec.stack --heap=no --stacks=yes ./PQCgenKAT_kem_dec
-echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind dec done
-mv massif.* ../../testresult/BIG_QUAKE_5/
+if [ $doValgrindFull == "1" ]; then
+  valgrind -q --tool=massif --massif-out-file=massif.out.full.heap --heap=yes --stacks=no ./PQCgenKAT_kem
+  valgrind -q --tool=massif --massif-out-file=massif.out.full.stack --heap=no --stacks=yes ./PQCgenKAT_kem
+  echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind full done
+fi
+if [ $doValgrindKeygen == "1" ]; then
+  valgrind -q --tool=massif --massif-out-file=massif.out.keygen.heap --heap=yes --stacks=no ./PQCgenKAT_kem_keygen
+  valgrind -q --tool=massif --massif-out-file=massif.out.keygen.stack --heap=no --stacks=yes ./PQCgenKAT_kem_keygen
+  echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind keygen done
+fi
+if [ $doValgrindEncDec == "1" ]; then
+  valgrind -q --tool=massif --massif-out-file=massif.out.enc.heap --heap=yes --stacks=no ./PQCgenKAT_kem_enc
+  valgrind -q --tool=massif --massif-out-file=massif.out.enc.stack --heap=no --stacks=yes ./PQCgenKAT_kem_enc
+  echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind enc done
+  valgrind -q --tool=massif --massif-out-file=massif.out.dec.heap --heap=yes --stacks=no ./PQCgenKAT_kem_dec
+  valgrind -q --tool=massif --massif-out-file=massif.out.dec.stack --heap=no --stacks=yes ./PQCgenKAT_kem_dec
+  echo `date +'%d.%m.%Y %H:%M:%S.%3N'` - valgrind dec done
+fi
+mv PQCkemKAT.req ../../../testresult/BIG_QUAKE_5/
+mv PQCkemKAT*.rsp ../../../testresult/BIG_QUAKE_5/
+mv massif.* ../../../testresult/BIG_QUAKE_5/ 2> /dev/null
 cd ../..
 
+echo $(date +'%d.%m.%Y %H:%M:%S.%3N') - end
 echo "done BIG_QUAKE"
-date +'%d.%m.%Y %H:%M:%S.%3N'
