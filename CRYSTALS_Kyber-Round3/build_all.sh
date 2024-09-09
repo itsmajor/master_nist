dcc44d88d3f73611f4df2979880390adaf16b550
Tomas <tomas.itsmajor@gmail.com>
2024-09-09 23:13:39 +0200

    CRYSTALS_Kyber 4kem

--- CRYSTALS_Kyber-Round3/build_all.sh
+++ CRYSTALS_Kyber-Round3/build_all.sh
@@ -1,41 +1,33 @@
 MAKEOPTION=$1;
 
-mkdir -p tests
-
 cd Optimized_Implementation/crypto_kem/kyber1024
 echo "moved to: ${PWD}"
 make $MAKEOPTION
-cp PQCgenKAT_kem ../../../tests/PQCgenKAT_kem__kyber1024
 cd ../../..
 
 cd Optimized_Implementation/crypto_kem/kyber1024-90s
 echo "moved to: ${PWD}"
 make $MAKEOPTION
-cp PQCgenKAT_kem ../../../tests/PQCgenKAT_kem__kyber1024-90s
 cd ../../..
 
 cd Optimized_Implementation/crypto_kem/kyber512
 echo "moved to: ${PWD}"
 make $MAKEOPTION
-cp PQCgenKAT_kem ../../../tests/PQCgenKAT_kem__kyber512
 cd ../../..
 
 cd Optimized_Implementation/crypto_kem/kyber512-90s
 echo "moved to: ${PWD}"
 make $MAKEOPTION
-cp PQCgenKAT_kem ../../../tests/PQCgenKAT_kem__kyber512-90s
 cd ../../..
 
 cd Optimized_Implementation/crypto_kem/kyber768
 echo "moved to: ${PWD}"
 make $MAKEOPTION
-cp PQCgenKAT_kem ../../../tests/PQCgenKAT_kem__kyber768
 cd ../../..
 
 cd Optimized_Implementation/crypto_kem/kyber768-90s
 echo "moved to: ${PWD}"
 make $MAKEOPTION
-cp PQCgenKAT_kem ../../../tests/PQCgenKAT_kem__kyber768-90s
 cd ../../..
 
 
