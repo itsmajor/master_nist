b96911b83406579e38f26c3b3659d4bbd7c8f81c
Tomas <tomas.itsmajor@gmail.com>
2024-09-12 22:50:25 +0200

    NTRU Round3 4kem

--- NTRU-Round3/build_all.sh
+++ NTRU-Round3/build_all.sh
@@ -1,32 +1,21 @@
 MAKEOPTION=$1;
 
-mkdir -p tests
-mkdir -p tests/ntruhrss701
-mkdir -p tests/ntruhps2048509
-mkdir -p tests/ntruhps2048677
-mkdir -p tests/ntruhps4096821
-
-
 cd Optimized_Implementation/crypto_kem/ntruhrss701
 echo "moved to: ${PWD}"
 make $MAKEOPTION
-mv PQCgenKAT_kem ../../../tests/ntruhrss701/
 cd ../../..
 
 cd Optimized_Implementation/crypto_kem/ntruhps2048509
 echo "moved to: ${PWD}"
 make $MAKEOPTION
-mv PQCgenKAT_kem ../../../tests/ntruhps2048509/
 cd ../../..
 
 cd Optimized_Implementation/crypto_kem/ntruhps2048677
 echo "moved to: ${PWD}"
 make $MAKEOPTION
-mv PQCgenKAT_kem ../../../tests/ntruhps2048677/
 cd ../../..
 
 cd Optimized_Implementation/crypto_kem/ntruhps4096821
 echo "moved to: ${PWD}"
 make $MAKEOPTION
-mv PQCgenKAT_kem ../../../tests/ntruhps4096821/
 cd ../../..
