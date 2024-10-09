2c2ed2f6e7e9b2684e68eb15ae2e4ca6cc7fc8e5
Tomas <tomas.itsmajor@gmail.com>
2024-10-09 22:11:55 +0200

    daemon sh

--- start_daemon.sh
+++ start_daemon.sh
@@ -1,3 +1,4 @@
-./test_all.sh -v > test_all.log &
+mkdir -p testresult
+./test_all.sh -v > testresult/test_all.log &
 #./test_some.sh -v > test_some.log &
 #./test_longrunning.sh -v > test_longrunning.log &
