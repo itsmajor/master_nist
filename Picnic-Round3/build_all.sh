4aadac11c7483c24181f32699a955def4cea311f
Tomas <tomas.itsmajor@gmail.com>
2024-10-12 16:32:53 +0200

    picnic generic32 add OI

--- Picnic-Round3/build_all.sh
+++ Picnic-Round3/build_all.sh
@@ -3,7 +3,7 @@ MAKEOPTION=$1;
 sec_array=('3l1' '3l3' '3l5' 'l1fs' 'l1full' 'l1ur' 'l3fs' 'l3full' 'l3ur' 'l5fs' 'l5full' 'l5ur')
 
 for sec in "${sec_array[@]}"; do
-  cd Additional_Implementations/picnic"$sec"-neon
+  cd Optimized_Implementation/picnic"$sec"
   echo "moved to: ${PWD}"
   make $MAKEOPTION
   cd ~-
--- Picnic-Round3/test_all.sh
+++ Picnic-Round3/test_all.sh
@@ -5,7 +5,7 @@
 sec_array=('3l1' '3l3' '3l5' 'l1fs' 'l1full' 'l1ur' 'l3fs' 'l3full' 'l3ur' 'l5fs' 'l5full' 'l5ur')
 
 for sec in "${sec_array[@]}"; do
-  CIPHER="sign Picnic_"$sec"_neon"
-  ../_common/script/doKat.sh $VALGRIND $CIPHER Additional_Implementations/picnic"$sec"-neon $OPTIONS
+  CIPHER="sign Picnic_"$sec"_OI"
+  ../_common/script/doKat.sh $VALGRIND $CIPHER Optimized_Implementation/picnic"$sec" $OPTIONS
   ../_common/script/doVerifyKat.sh $CIPHER $DEBUG_VERIFYKAT
 done
