cf25a5f614a2ec7e6cfbf5c6dc756a7ac09b5471
Tomas <tomas.itsmajor@gmail.com>
2024-10-12 14:57:36 +0200

    daemon scripts

---
+++ clean_testresults.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+rm -f -R testresult/
+echo "removed testresult/"
--- start_daemon.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p testresult
-./test_all.sh -v > testresult/test_all.log &
-#./test_some.sh -v > test_some.log &
-#./test_longrunning.sh -v > test_longrunning.log &
