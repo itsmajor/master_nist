cc9716bbda227e7303accf41fa8ea8888b874a04
Tomas <tomas.itsmajor@gmail.com>
2024-10-17 22:54:25 +0200

    update start daemon

--- start_daemon_test_all.sh
+++ start_daemon_test_all.sh
@@ -9,5 +9,6 @@ fi
 ./clean_testresults.sh
 mkdir -p testresult
 touch testresult/host_`hostname`
-./test_all.sh $PARAM > testresult/test_all.log 2>&1 &
+echo "test options was '"$@"'" > testresult/test_all.log
+./test_all.sh $PARAM >> testresult/test_all.log 2>&1 &
 tail -f testresult/test_all.log
