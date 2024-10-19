2aad99429643de38fec98f7bcd2de7a91759cf0c
Tomas <tomas.itsmajor@gmail.com>
2024-10-20 00:12:19 +0200

    fix move to majorpc
    (cherry picked from commit 0bf6edfb00069a60f8ebe706cd3643218a78b0d3)

--- move_to_majorpc.sh
+++ move_to_majorpc.sh
@@ -3,7 +3,7 @@
 if [ ! -d "/mnt/majorpc/testresult_final/" ]; then
   echo "creating mount to majorpc"
   sudo mkdir -p /mnt/majorpc/testresult_final
-  sudo chmod 666 fstab
+  sudo chmod 666 /etc/fstab
   echo -e "\n\n#MajorPC\n//192.168.0.100/testresult_final /mnt/majorpc/testresult_final cifs username=major,iocharset=utf8,file_mode=0777,dir_mode=0777,noperm 0 0" >> /etc/fstab
   sudo mount -a
 fi
