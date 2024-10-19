c38d3ac45df44adb169661ed35d697fa1622cbf4
Tomas <tomas.itsmajor@gmail.com>
2024-10-19 17:59:39 +0200

    move to majorpc
    (cherry picked from commit 8bb341ccdc3c3cfae2772ff68bdcacd45ba43db8)

--- move_to_majorpc.sh
+++ move_to_majorpc.sh
@@ -4,9 +4,11 @@ if [ ! -d "/mnt/majorpc/testresult_final/" ]; then
   echo "creating mount to majorpc"
   sudo mkdir -p /mnt/majorpc/testresult_final
   sudo chmod 666 fstab
-  sudo echo -e "\n\n#MajorPC\n//192.168.0.100/testresult_final /mnt/majorpc/testresult_final cifs username=major" >> /etc/fstab
+  echo -e "\n\n#MajorPC\n//192.168.0.100/testresult_final /mnt/majorpc/testresult_final cifs username=major,iocharset=utf8,file_mode=0777,dir_mode=0777,noperm 0 0" >> /etc/fstab
   sudo mount -a
 fi
 
-mv testresult_* /mnt/majorpc/testresult_final/ &
-echo "move done (testresult to majorpc/testresult_final) "
+TESTPATH=`ls | grep testresult_`
+echo "moving $TESTPATH to majorpc"
+mv testresult_* /mnt/majorpc/testresult_final/
+echo "move done ($TESTPATH)"
