bf636464bc05782df4d1c6f17c2ef82914d8977f
Tomas <tomas.itsmajor@gmail.com>
2024-10-22 22:12:15 +0200

    testresult cat grep -a

--- rename_testresult.sh
+++ rename_testresult.sh
@@ -2,11 +2,13 @@
 
 cd testresult
 if [ -f "test_all.log" ]; then
-  cat test_all.log | grep -i "error" | grep -v "not equal" | grep -i -v "known error" | grep -i -v "skip " > test_all_errors.log
+  cat test_all.log | grep -i -a "error" | grep -v -a "not equal" | grep -i -v -a "known error" | grep -i -v -a "skip " > test_all_errors.log
 fi
 if [ -f "verifyresult.log" ]; then
-  cat verifyresult.log | grep -i error > verifyresult_errors.log
+  cat verifyresult.log | grep -i -a "error" > verifyresult_errors.log
 fi
 cd ..
+
+
 
 mv testresult testresult_`hostname`_$(date +'%Y.%m.%d_%H.%M')_`echo $@ | sed 's/ /_/g'`
