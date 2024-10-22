8fd31b6a88cbb2898b75a94aca2b515ab26ab9ef
Tomas <tomas.itsmajor@gmail.com>
2024-10-22 22:55:04 +0200

    dummy randombytes.h
    (cherry picked from commit 4665c90f0d5c72ef705f613697ea913fe4e5ac79)

---
+++ _common/NIST/randombytes.h
@@ -0,0 +1,8 @@
+//
+// Created by Major on 22.10.2024.
+// dummy file - randombytes() is in rng.h
+
+#ifndef MASTER_NIST_RANDOMBYTES_H
+#define MASTER_NIST_RANDOMBYTES_H
+
+#endif //MASTER_NIST_RANDOMBYTES_H
