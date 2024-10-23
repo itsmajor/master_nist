8a85404c8260c3357eccc3de8ba6d97c9c2afd58
Tomas <tomas.itsmajor@gmail.com>
2024-10-23 23:47:35 +0200

    frodo add shake
    (cherry picked from commit ab30dea26496c4badce8d5a4c2edea636c8e1fb7)

--- FrodoKEM-Round3/build_all.sh
+++ FrodoKEM-Round3/build_all.sh
@@ -1,17 +1,12 @@
 MAKEOPTION=$1;
 
-cd Optimized_Implementation/optimized/FrodoKEM-640
-echo "moved to: ${PWD}"
-make $MAKEOPTION
-cd ~-
+sec_array=(640 976 1344)
 
-cd Optimized_Implementation/optimized/FrodoKEM-976
-echo "moved to: ${PWD}"
-make $MAKEOPTION
-cd ~-
-
-cd Optimized_Implementation/optimized/FrodoKEM-1344
-echo "moved to: ${PWD}"
-make $MAKEOPTION
-cd ~-
+for sec in "${sec_array[@]}"; do
+  cd Optimized_Implementation/optimized/FrodoKEM-$sec
+  echo "moved to: ${PWD}"
+  make SEC=$sec MATRIX=AES $MAKEOPTION
+  make SEC=$sec MATRIX=SHAKE $MAKEOPTION
+  cd ~-
+done
 
