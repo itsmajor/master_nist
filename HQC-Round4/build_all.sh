62e4177934be326f4db7efa594a974ddedd8a2c0
Tomas <tomas.itsmajor@gmail.com>
2024-10-25 01:35:47 +0200

    HQC switch 32bit AI -> RI

--- HQC-Round4/build_all.sh
+++ HQC-Round4/build_all.sh
@@ -1,16 +1,16 @@
 MAKEOPTION=$1;
 
-cd Additional_Implementation/hqc-128/src
+cd Reference_Implementation/hqc-128/src
 echo "moved to: ${PWD}"
 make $MAKEOPTION
 cd ~-
 
-cd Additional_Implementation/hqc-192/src
+cd Reference_Implementation/hqc-192/src
 echo "moved to: ${PWD}"
 make $MAKEOPTION
 cd ~-
 
-cd Additional_Implementation/hqc-256/src
+cd Reference_Implementation/hqc-256/src
 echo "moved to: ${PWD}"
 make $MAKEOPTION
 cd ~-
--- HQC-Round4/test_all.sh
+++ HQC-Round4/test_all.sh
@@ -3,13 +3,13 @@
 . ../_common/script/test_all_param.sh "$@"
 
 CIPHER="kem hqc-128"
-../_common/script/doKat.sh $VALGRIND $CIPHER Additional_Implementation/hqc-128/src $OPTIONS
+../_common/script/doKat.sh $VALGRIND $CIPHER Reference_Implementation/hqc-128/src $OPTIONS
 ../_common/script/doVerifyKat.sh $CIPHER $DEBUG_VERIFYKAT
 
 CIPHER="kem hqc-192"
-../_common/script/doKat.sh $VALGRIND $CIPHER Additional_Implementation/hqc-192/src $OPTIONS
+../_common/script/doKat.sh $VALGRIND $CIPHER Reference_Implementation/hqc-192/src $OPTIONS
 ../_common/script/doVerifyKat.sh $CIPHER $DEBUG_VERIFYKAT
 
 CIPHER="kem hqc-256"
-../_common/script/doKat.sh $VALGRIND $CIPHER Additional_Implementation/hqc-256/src $OPTIONS
+../_common/script/doKat.sh $VALGRIND $CIPHER Reference_Implementation/hqc-256/src $OPTIONS
 ../_common/script/doVerifyKat.sh $CIPHER $DEBUG_VERIFYKAT
