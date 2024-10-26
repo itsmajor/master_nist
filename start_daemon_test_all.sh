8befe2b36a6602e6de81b8dc33ba94df7d0c7971
Tomas <tomas.itsmajor@gmail.com>
2024-10-26 16:14:58 +0200

    copy build log in testresult

--- start_daemon_test_all.sh
+++ start_daemon_test_all.sh
@@ -8,6 +8,7 @@ fi
 
 ./clean_testresults.sh
 mkdir -p testresult
+cp build_all.log testresult/ 2> /dev/null
 touch testresult/host_`hostname`
 echo "test options was '"$@"'" > testresult/test_all.log
 ./test_all.sh $PARAM >> testresult/test_all.log 2>&1 &
