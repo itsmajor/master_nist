426fa04a181a83ca1bf4a387ec3ecbcf93e91b8c
Tomas <tomas.itsmajor@gmail.com>
2024-10-27 21:54:16 +0100

    liboqs run

--- _common/liboqs/run_liboqs_tests.sh
+++ _common/liboqs/run_liboqs_tests.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ ! -d "~/workspace/liboqs/build" ]
-  echo "~/workspace/liboqs/build not found - run prepare_liboqs.sh first"
+if [ ! -d "`eval echo ~/workspace/liboqs/build/`" ]; then
+  echo "*** starting ./prepare_liboqs.sh because ~/workspace/liboqs/build/ not found"
+  ./prepare_liboqs.sh
+  echo "*** done ./prepare_liboqs.sh"
 fi
 
 
-cd ~/workspace/liboqs/build
+python3 run_mem_tomas.py /home/major/workspace/liboqs/build/tests/test_kem_mem
 
-#complete test suite
-ninja run_tests
+mkdir -p ../../testresult_liboqs_`hostname`_$(date +'%Y.%m.%d_%H.%M')
