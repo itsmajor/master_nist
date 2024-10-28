1174b9c01d3fff7a8ed4349677387763dc09c4a6
Tomas <tomas.itsmajor@gmail.com>
2024-10-28 17:24:12 +0100

    liboqs path

--- _common/liboqs/test_liboqs_mem.sh
+++ _common/liboqs/test_liboqs_mem.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-sudo mount -a
+LIBOQSPATH=`eval echo ~/workspace/liboqs_backup/build/tests`
+TESTRESULTPATH=../../testresult_liboqs_`hostname`_$(date +'%Y.%m.%d_%H.%M')
 
-if [ ! -d "`eval echo ~/workspace/liboqs/build/tests`" ]; then
-  echo "*** starting ./prepare_liboqs.sh  (~/workspace/liboqs/build/tests not found)"
+if [ ! -d "$LIBOQSPATH" ]; then
+  echo "*** starting ./prepare_liboqs.sh  ($LIBOQSPATH not found)"
   ./prepare_liboqs.sh
   echo "*** done ./prepare_liboqs.sh"
 fi
 
 echo "starting liboqs tests"
-script -c "~/workspace/liboqs/build/tests/speed_kem" liboqs_kem_speed.log
-script -c "~/workspace/liboqs/build/tests/speed_sig" liboqs_sig_speed.log
-script -c "python3 run_mem_tomas.py ~/workspace/liboqs/build/tests/test_kem_mem" liboqs_kem_mem.log
-script -c "python3 run_mem_tomas.py ~/workspace/liboqs/build/tests/test_sig_mem" liboqs_sig_mem.log
+script -c "$LIBOQSPATH/speed_kem" liboqs_kem_speed.log
+script -c "$LIBOQSPATH/tests/speed_sig" liboqs_sig_speed.log
+script -c "python3 run_mem_tomas.py $LIBOQSPATH/test_kem_mem" liboqs_kem_mem.log
+script -c "python3 run_mem_tomas.py $LIBOQSPATH/test_sig_mem" liboqs_sig_mem.log
 
-TESTPATH=../../testresult_liboqs_`hostname`_$(date +'%Y.%m.%d_%H.%M')
-mkdir -p $TESTPATH
-mv liboqs_*.log $TESTPATH/
-touch $TESTPATH/host_`hostname`
-mv ~/workspace/liboqs/build/tests/test_*_mem.json $TESTPATH/
-mv valgrind-out $TESTPATH/
+mkdir -p $TESTRESULTPATH
+mv liboqs_*.log $TESTRESULTPATH/
+touch $TESTRESULTPATH/host_`hostname`
+mv $LIBOQSPATH/test_*_mem.json $TESTRESULTPATH/
+mv valgrind-out $TESTRESULTPATH/
 
 cd ../../
 ./move_to_majorpc.sh
