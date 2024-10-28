27a0b3cde2e0636e89e321f5233175340009560b
Tomas <tomas.itsmajor@gmail.com>
2024-10-28 01:21:45 +0100

    liboqs
    (cherry picked from commit 51ca1fc13faacdd71fe1b0a3aa34d487f537538d)

--- _common/liboqs/test_liboqs_mem.sh
+++ _common/liboqs/test_liboqs_mem.sh
@@ -2,20 +2,23 @@
 
 sudo mount -a
 
-if [ ! -d "`eval echo ~/workspace/liboqs/build/`" ]; then
-  echo "*** starting ./prepare_liboqs.sh because ~/workspace/liboqs/build/ not found"
+if [ ! -d "`eval echo ~/workspace/liboqs/build/tests`" ]; then
+  echo "*** starting ./prepare_liboqs.sh  (~/workspace/liboqs/build/tests not found)"
   ./prepare_liboqs.sh
   echo "*** done ./prepare_liboqs.sh"
 fi
 
-echo "starting liboqs mem tests - this will take a while (30s+) until first result is shown"
-script -c "python3 run_mem_tomas.py ~/workspace/liboqs/build/tests/test_kem_mem" liboqs_mem.log
+echo "starting liboqs tests"
+script -c "~/workspace/liboqs/build/tests/speed_kem" liboqs_kem_speed.log
+script -c "~/workspace/liboqs/build/tests/speed_sig" liboqs_sig_speed.log
+script -c "python3 run_mem_tomas.py ~/workspace/liboqs/build/tests/test_kem_mem" liboqs_kem_mem.log
+script -c "python3 run_mem_tomas.py ~/workspace/liboqs/build/tests/test_sig_mem" liboqs_sig_mem.log
 
 TESTPATH=../../testresult_liboqs_`hostname`_$(date +'%Y.%m.%d_%H.%M')
 mkdir -p $TESTPATH
-mv liboqs_mem.log $TESTPATH/
+mv liboqs_*.log $TESTPATH/
 touch $TESTPATH/host_`hostname`
-mv ~/workspace/liboqs/build/tests/test_kem_mem.json $TESTPATH/
+mv ~/workspace/liboqs/build/tests/test_*_mem.json $TESTPATH/
 mv valgrind-out $TESTPATH/
 
 cd ../../
