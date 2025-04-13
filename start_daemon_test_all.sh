56d4458a52f3dd2abb1b3377e476a4f81680ba72
Tomas <tomas.itsmajor@gmail.com>
2025-04-13 13:37:14 +0200

    tail verifyresult on daemon_test

--- start_daemon_test_all.sh
+++ start_daemon_test_all.sh
@@ -10,6 +10,9 @@ fi
 mkdir -p testresult
 cp build_all.log testresult/ 2> /dev/null
 touch testresult/host_`hostname`
+touch testresult/verifyresult.log
 echo "test options was '"$@"'" > testresult/test_all.log
 ./test_all.sh $PARAM >> testresult/test_all.log 2>&1 &
-tail -f testresult/test_all.log
+
+#tail -f testresult/test_all.log
+tail -f testresult/verifyresult.log
