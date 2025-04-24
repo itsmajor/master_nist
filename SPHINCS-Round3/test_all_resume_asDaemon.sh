46a00ff9a5dd47a9b59a2af3271d143905810fd0
Tomas <tomas.itsmajor@gmail.com>
2025-04-24 02:53:26 +0200

    update resume sphincs

--- SPHINCS-Round3/test_all_resume_asDaemon.sh
+++ SPHINCS-Round3/test_all_resume_asDaemon.sh
@@ -3,3 +3,6 @@
 . ../_common/script/test_all_param.sh "$@"
 
 nohup ./test_all_resume.sh > ../testresult/sphincs_resume_log.txt 2>&1 &
+echo "Daemon sphincs_resume gestartet mit PID $!"
+echo "starte tail -f ../testresult/sphincs_resume_log.txt:"
+tail -f ../testresult/sphincs_resume_log.txt
