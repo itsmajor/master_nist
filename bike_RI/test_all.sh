8fe40cc1de82c57ec461f2fa8ee0fea835bdc1a8
Tomas <tomas.itsmajor@gmail.com>
2025-04-28 15:44:04 +0200

    align bike 32bit with 64bit naming

--- bike_RI/test_all.sh
+++ bike_RI/test_all.sh
@@ -3,11 +3,11 @@
 . ../_common/script/test_all_param.sh "$@"
 
 mode_array=(1 3 5)
-hash_array=('AES' 'SHA3')
+hash_array=('aes' 'sha3')
 
 for mode in "${mode_array[@]}"; do
   for hash in "${hash_array[@]}"; do
-    CIPHER="kem BIKE_L"$mode"_"$hash
+    CIPHER="kem bike_"$mode"_"$hash
     ../_common/script/doKat.sh $VALGRIND $CIPHER bin/"$hash"_"$mode" $OPTIONS
     ../_common/script/doVerifyKat.sh $CIPHER $DEBUG_VERIFYKAT
   done
