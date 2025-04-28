f94053a990d3d67c901029ea5b4606e0100cdf5b
Tomas <tomas.itsmajor@gmail.com>
2025-04-28 14:31:35 +0200

    fix bike 32bit

--- bike_RI/test_all.sh
+++ bike_RI/test_all.sh
@@ -8,7 +8,7 @@ hash_array=('AES' 'SHA3')
 for mode in "${mode_array[@]}"; do
   for hash in "${hash_array[@]}"; do
     CIPHER="kem BIKE_L"$mode"_"$hash
-    ../_common/script/doKat.sh $VALGRIND $CIPHER bin/BIKE_L"$mode"_"$hash" $OPTIONS
+    ../_common/script/doKat.sh $VALGRIND $CIPHER bin/"$hash"_"$mode" $OPTIONS
     ../_common/script/doVerifyKat.sh $CIPHER $DEBUG_VERIFYKAT
   done
 done
