b7dc99d668304673b25872af0dcad457cd605dc7
Tomas <tomas.itsmajor@gmail.com>
2025-05-04 20:16:56 +0200

    fix 32bit KINDI inline asm, add -lm flag

--- KINDI-Round1/Reference_Implementation/kem/KINDI-256-3-4-2/util.c
+++ KINDI-Round1/Reference_Implementation/kem/KINDI-256-3-4-2/util.c
@@ -3,7 +3,7 @@
 long long cpucycles(void) {
 
 	unsigned long long result;
-	asm volatile("isb; mrs %0, CNTVCT_EL0" : "=r"(result));
+//	asm volatile("isb; mrs %0, CNTVCT_EL0" : "=r"(result));
 	return result;
 }
 
