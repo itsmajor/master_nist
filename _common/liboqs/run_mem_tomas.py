e33980d5358f94169332a8758b7515c773d7bf6e
Tomas <tomas.itsmajor@gmail.com>
2025-05-11 05:11:03 +0200

    cleanup run_mem_tomas

--- _common/liboqs/run_mem_tomas.py
+++ _common/liboqs/run_mem_tomas.py
@@ -54,28 +54,6 @@ def parse_config(output):
 fieldname = ["insts", "maxBytes", "maxHeap", "extHeap", "maxStack"]
 
 
-# def do_test(alg, meth, methnames, exepath):
-#     process = subprocess.Popen(["valgrind", "--tool=massif", "--stacks=yes", "--massif-out-file=valgrind-out", exepath, alg, str(meth)], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-#     (outs, errs) = process.communicate()
-#     if process.returncode != 0:
-#         print("* Result for %s_%d: %s %d" % (alg, meth, "Valgrind died with", process.returncode))
-#         return
-#         # print("Valgrind died with retcode %d and \n%s\n%s\nFatal error. Exiting." % (process.returncode, outs, errs))
-#         # exit(1)
-#     if len(data["config"]) == 0:
-#         parse_config(outs)
-#     process = subprocess.Popen(["ms_print", "valgrind-out"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
-#     (outs, errs) = process.communicate()
-#     result = get_peak(outs.splitlines())
-#     data[alg][methnames[meth]] = {}
-#     try:
-#         print("Result for %s %d (%s): %s" % (alg, meth, methnames[meth], " ".join(result)))
-#         for i in range(5):
-#             data[alg][methnames[meth]][fieldname[i]] = result[i]
-#     except TypeError:
-#         print("Result for %s %d: " % (alg, meth))
-#         print(result)
-#         print(outs.splitlines())
 def do_test(alg, meth, methnames, exepath):
     process = subprocess.Popen(
         ["valgrind", "--tool=massif", "--stacks=yes", "--massif-out-file=valgrind-out", exepath, alg, str(meth)],
