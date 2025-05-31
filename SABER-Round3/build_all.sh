8f5e4e7fb1477dc11cdfc0b5f2f091c69ea58fd2
Tomas <tomas.itsmajor@gmail.com>
2025-05-31 22:53:49 +0200

    rework saber build/test_all

--- SABER-Round3/build_all.sh
+++ SABER-Round3/build_all.sh
@@ -2,5 +2,7 @@ MAKEOPTION=$1;
 
 cd Reference_Implementation_KEM
 echo "moved to: ${PWD}"
-make $MAKEOPTION
+make saber2 $MAKEOPTION
+make saber3 $MAKEOPTION
+make saber4 $MAKEOPTION
 cd ~-
--- SABER-Round3/test_all.sh
+++ SABER-Round3/test_all.sh
@@ -2,6 +2,14 @@
 
 . ../_common/script/test_all_param.sh "$@"
 
+CIPHER="kem LightSABER"
+../_common/script/doKat.sh $VALGRIND $CIPHER bin/SABER_L-2 $OPTIONS
+../_common/script/doVerifyKat.sh $CIPHER $DEBUG_VERIFYKAT
+
 CIPHER="kem SABER"
-../_common/script/doKat.sh $VALGRIND $CIPHER Reference_Implementation_KEM $OPTIONS
+../_common/script/doKat.sh $VALGRIND $CIPHER bin/SABER_L-3 $OPTIONS
+../_common/script/doVerifyKat.sh $CIPHER $DEBUG_VERIFYKAT
+
+CIPHER="kem FireSABER"
+../_common/script/doKat.sh $VALGRIND $CIPHER bin/SABER_L-4 $OPTIONS
 ../_common/script/doVerifyKat.sh $CIPHER $DEBUG_VERIFYKAT
