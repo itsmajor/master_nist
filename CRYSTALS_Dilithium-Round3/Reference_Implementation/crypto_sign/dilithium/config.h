a608a12bbaf64a9838f2b31cb0674b49d8638dd4
Tomas <tomas.itsmajor@gmail.com>
2025-06-03 13:19:44 +0200

    remove #define DILITHIUM_MODE 2

--- CRYSTALS_Dilithium-Round3/Reference_Implementation/crypto_sign/dilithium/config.h
+++ CRYSTALS_Dilithium-Round3/Reference_Implementation/crypto_sign/dilithium/config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define DILITHIUM_MODE 2
+//#define DILITHIUM_MODE 2
 
 #define CRYPTO_ALGNAME "Dilithium2"
 #define DILITHIUM_NAMESPACE(s) pqcrystals_dilithium2_ref##s
